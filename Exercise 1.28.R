26aca31be366b706252ceffd85810db134b1ccd0
5H4N3O <shanepratt2002@gmail.com>
2025-01-27 17:22:33 -0600

    Load data, summarize, and plot with regression line

---
+++ Exercise 1.28.R
@@ -0,0 +1,15 @@
+# Load Data
+crimerate <- read.table("CH01PR28.txt", header = FALSE, sep = "")
+colnames(crimerate) <-c("Y","X")
+crimerate <- as.data.frame(crimerate)
+
+# View Data
+head(crimerate)
+
+# Summarize Data
+model <- lm(formula = Y ~ X, data = crimerate)
+summary(model)
+
+# Scatter plot with regression line
+plot(crimerate$X,crimerate$Y, xlab="% of Individuals With a High School Diploma",ylab="Crime Rate (crimes reported per 100,000 residents)")
+abline(lm(formula = Y ~ X, data = crimerate), col = "blue")
