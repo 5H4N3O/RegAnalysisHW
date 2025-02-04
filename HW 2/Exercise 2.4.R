a4b205c91a53ebfbdd59551561643386014bbfe5
5H4N3O <shanepratt2002@gmail.com>
2025-02-04 11:59:48 -0600

    Added calculations for beta1 confidence interval

--- HW 2/Exercise 2.4.R
+++ HW 2/Exercise 2.4.R
@@ -5,4 +5,25 @@ gpa <- as.data.frame(gpa)
 
 # Summarize Data
 model <- lm(formula = Y ~ X, data = gpa)
-summary(model)
+summary <- summary(model)
+summary
+
+# Store data in Variables
+b0 <- coef(model)[1]
+Sb0 <- summary$coefficients[1, 2] 
+
+b1 <- coef(model)[2]
+Sb1 <- summary$coefficients[2, 2] 
+
+# Get 99% Confidence Interval for β1 Manually
+n<-nrow(gpa)
+alpha <- 0.01
+tval <- qt(1-alpha/2, n-2)
+lowerb <- (b1-tval*Sb1)
+upperb <- (b1+tval*Sb1) 
+confint <- paste0("[", lowerb, ", ", upperb, "]")
+print(confint)
+
+# Get 99% Confidence Interval for β1 with R function
+ci_b1 <- confint(model, level = 0.99)
+print(ci_b1)
