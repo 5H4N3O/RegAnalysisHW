bb3257885a302b5e7d40fa9faa1c48c7a9499b88
5H4N3O <shanepratt2002@gmail.com>
2025-02-11 19:14:30 -0600

    Removed unnecessary variables in order to condense output

--- HW 3/Exercise 2.23.R
+++ HW 3/Exercise 2.23.R
@@ -8,11 +8,5 @@ model <- lm(formula = Y ~ X, data = gpa)
 summary <- summary(model)
 summary
 
-# Store data in Variables
-b0 <- coef(model)[1]
-Sb0 <- summary$coefficients[1, 2] 
-b1 <- coef(model)[2]
-Sb1 <- summary$coefficients[2, 2] 
-
 # Set up ANOVA table
 anova(model)
--- HW 3/Exercise 2.26.R
+++ HW 3/Exercise 2.26.R
@@ -8,12 +8,6 @@ model <- lm(formula = Y ~ X, data = hardness)
 summary <- summary(model)
 summary
 
-# Store data in Variables
-b0 <- coef(model)[1]
-Sb0 <- summary$coefficients[1, 2] 
-b1 <- coef(model)[2]
-Sb1 <- summary$coefficients[2, 2] 
-
 # Set up ANOVA table
 anova(model)
 
