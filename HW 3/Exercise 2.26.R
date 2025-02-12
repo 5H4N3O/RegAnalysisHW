5489bb9b70b5ff69412bddf334ddcf84129cbceb
5H4N3O <shanepratt2002@gmail.com>
2025-02-11 18:43:51 -0600

    Create file for exercise 2.26, set up data, summary table, and ANOVA table

---
+++ HW 3/Exercise 2.26.R
@@ -0,0 +1,18 @@
+# Load Data
+hardness <- read.table("../datasets/CH01PR22.txt", header = FALSE, sep = "")
+colnames(hardness) <-c("Y","X")
+hardness <- as.data.frame(hardness)
+
+# Summarize Data
+model <- lm(formula = Y ~ X, data = hardness)
+summary <- summary(model)
+summary
+
+# Store data in Variables
+b0 <- coef(model)[1]
+Sb0 <- summary$coefficients[1, 2] 
+b1 <- coef(model)[2]
+Sb1 <- summary$coefficients[2, 2] 
+
+# Set up ANOVA table
+anova(model)
