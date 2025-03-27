d99518e0c1fde63408b69a63eaa2539907fc36c6
5H4N3O <shanepratt2002@gmail.com>
2025-03-26 21:03:42 -0500

    Added dataset and initial setup of exercise 6.9

---
+++ HW 6/6.9.R
@@ -0,0 +1,13 @@
+# Load data
+my.data <- read.table("../datasets/CH06PR09.txt", header = FALSE, sep = "")
+head(my.data)
+
+colnames(my.data) <-c("Y","X1","X2","X3")
+my.data <- as.data.frame(my.data)
+n <- nrow(my.data)
+
+#Scatter Plot
+plot(my.data)
+
+#Correlation Matrix
+cor(my.data)
